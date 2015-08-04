ecea780df2e79ccee8bb31c2c37578c56b350277
Alex Atmore <acatmore@gmail.com>
2015-08-03 19:38:09 -0500

    added tabs and ability to hide completed tasks

--- meteorbloclist.js
+++ meteorbloclist.js
@@ -4,8 +4,20 @@ if (Meteor.isClient) {
   // counter starts at 0
  Template.body.helpers({
     tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
           // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
+          },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
  });
  
@@ -25,9 +37,12 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
-   
+
   Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
@@ -39,6 +54,40 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
+
+  ReactiveTabs.createInterface({
+  template: 'basicTabs',
+  onChange: function (slug, template) {
+    // This callback runs every time a tab changes.
+    // The `template` instance is unique per {{#basicTabs}} block.
+    console.log('[tabs] Tab has changed! Current tab:', slug);
+    console.log('[tabs] Template instance calling onChange:', template);
+  }
+});
+  Template.myTabs.helpers({
+  tabs: function () {
+    // Every tab object MUST have a name and a slug!
+    return [
+      { name: 'Active', slug: 'active' },
+      { name: 'Expired', slug: 'expired' },
+      { name: 'Completed', slug: 'completed', onRender: function(slug, template) {
+        // This callback runs every time this specific tab's content renders.
+        // As with `onChange`, the `template` instance is unique per block helper.
+      }}
+    ];
+  },
+  activeTab: function () {
+    // Use this optional helper to reactively set the active tab.
+    // All you have to do is return the slug of the tab.
+
+    // You can set this using an Iron Router param if you want--
+    // or a Session variable, or any reactive value from anywhere.
+
+    // If you don't provide an active tab, the first one is selected by default.
+    // See the `advanced use` section below to learn about dynamic tabs.
+    return Session.get('activeTab'); // Returns "people", "places", or "things".
+  }
+});
 }
 
 if (Meteor.isServer) {
